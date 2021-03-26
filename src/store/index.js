84f35be6aa3b7f31a6f993e1ffbb709a21440dfa
Alex Abbott <Robert.Abbott@henryschein.com>
2021-03-25 21:34:19 -0600

    Fixed issue with npm build, added some things to the store

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,13 @@ export default new Vuex.Store({
   state: {
     darkMode: true,
     projects: [
-      {id: 0, img: '', name: '', github: '', demo: ''},
-      {id: 1, img: '', name: '', github: '', demo: ''},
-      {id: 2, img: '', name: '', github: '', demo: ''},
-      {id: 3, img: '', name: '', github: '', demo: ''},
-      {id: 4, img: '', name: '', github: '', demo: ''},
-      {id: 5, img: '', name: '', github: '', demo: ''},
+      {id: 0, img: '', name: 'Paktola', github: '', demo: '', description: 'Co-founded'},
+      {id: 1, img: '', name: 'AirPass', github: '', demo: '', description: 'Co-founded'},
+      {id: 2, img: '', name: 'Inergy Survey', github: '', demo: ''},
+      {id: 3, img: '', name: 'Easy Order Manager', github: '', demo: ''},
+      {id: 4, img: '', name: 'NP Script', github: 'https://github.com/RAAbbott/npScript', demo: ''},
+      {id: 5, img: '', name: 'SimplyNote', github: '', demo: ''},
+      {id: 6, img: '', name: 'Portfolio', github: '', demo: ''},
     ]
   },
   getters: {
